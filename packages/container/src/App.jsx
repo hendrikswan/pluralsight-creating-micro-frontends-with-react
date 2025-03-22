f27cdd90bbe392ee1c934481e600ab8167e9be41
Hendrik Swanepoel <hendrik@canva.com>
2025-03-22 13:47:37 +1100

    Navigation

--- packages/container/src/App.jsx
+++ packages/container/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./app.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
 
 export default function App() {
   return (
     <BrowserRouter basename="/">
       <React.Suspense fallback={"Loading"}>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/products/*" element={<RemoteProducts />} />
