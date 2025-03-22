7d293d12e3e756f14c124a65b127403f8c4f7637
Hendrik Swanepoel <hendrik@canva.com>
2025-03-22 14:07:51 +1100

    Simplified bootstrap

--- packages/products/src/bootstrap.js
+++ packages/products/src/bootstrap.js
@@ -3,26 +3,10 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import Products from "./Products";
 
-const mount = (el) => {
-  const root = ReactDOM.createRoot(el);
-  root.render(
-    // Only use BrowserRouter when running standalone
-    window.location.port === "3001" ? (
-      <BrowserRouter>
-        <Products />
-      </BrowserRouter>
-    ) : (
-      <Products />
-    )
-  );
-};
-
-// If we are in development and running standalone
-if (process.env.NODE_ENV === "development") {
-  const el = document.getElementById("root");
-  if (el) {
-    mount(el);
-  }
-}
-
-export { mount };
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+  <BrowserRouter>
+    <Products />
+  </BrowserRouter>
+);
