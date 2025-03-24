0af473eca7816f01b6df2c1e43e1da1fbc39f9be
Hendrik Swanepoel <hendrik@canva.com>
2025-03-24 16:49:40 +1100

    Cart context integrated in products

--- packages/products/src/bootstrap.js
+++ packages/products/src/bootstrap.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Products from "./Products";
 import { BrowserRouter } from "react-router-dom";
+import { CartProvider } from "cart/CartContext";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(
   <BrowserRouter>
-    <Products />
+    <CartProvider>
+      <Products />
+    </CartProvider>
   </BrowserRouter>
 );
